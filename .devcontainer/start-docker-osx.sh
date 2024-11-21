09161d56f5998a06d23a5ab27689f1c43c687010
Ansuman Sahoo <anshumansahoo500@gmail.com>
2024-11-21 19:52:39 +0530

    Update

--- .devcontainer/start-docker-osx.sh
+++ .devcontainer/start-docker-osx.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-
-ls -a 
-
-docker run -i \
-    --device /dev/kvm \
-    -p 50922:10022 \
-    -p 5999:5999 \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e "DISPLAY=${DISPLAY:-:0.0}" \
-    -e EXTRA="-display none -vnc 0.0.0.0:99,password=off" \
-    -e SHORTNAME=ventura \
-    sickcodes/docker-osx:latest
+# CMD from the docker-osx Dockerfile
+! [[ -e "${BASESYSTEM_IMAGE:-BaseSystem.img}" ]] \
+    && printf '%s\n' "No BaseSystem.img available, downloading ${SHORTNAME}" \
+    && make \
+    && qemu-img convert BaseSystem.dmg -O qcow2 -p -c ${BASESYSTEM_IMAGE:-BaseSystem.img} \
+    && rm ./BaseSystem.dmg \
+; sudo touch /dev/kvm /dev/snd "${IMAGE_PATH}" "${BOOTDISK}" "${ENV}" 2>/dev/null || true \
+; sudo chown -R $(id -u):$(id -g) /dev/kvm /dev/snd "${IMAGE_PATH}" "${BOOTDISK}" "${ENV}" 2>/dev/null || true \
+; [[ "${NOPICKER}" == true ]] && { \
+    sed -i '/^.*InstallMedia.*/d' Launch.sh \
+    && export BOOTDISK="${BOOTDISK:=/home/arch/OSX-KVM/OpenCore/OpenCore-nopicker.qcow2}" \
+; } \
+|| export BOOTDISK="${BOOTDISK:=/home/arch/OSX-KVM/OpenCore/OpenCore.qcow2}" \
+; [[ "${GENERATE_UNIQUE}" == true ]] && { \
+    ./Docker-OSX/osx-serial-generator/generate-unique-machine-values.sh \
+        --master-plist-url="${MASTER_PLIST_URL}" \
+        --count 1 \
+        --tsv ./serial.tsv \
+        --bootdisks \
+        --width "${WIDTH:-1920}" \
+        --height "${HEIGHT:-1080}" \
+        --output-bootdisk "${BOOTDISK:=/home/arch/OSX-KVM/OpenCore/OpenCore.qcow2}" \
+        --output-env "${ENV:=/env}" \
+|| exit 1 ; } \
+; [[ "${GENERATE_SPECIFIC}" == true ]] && { \
+        source "${ENV:=/env}" 2>/dev/null \
+        ; ./Docker-OSX/osx-serial-generator/generate-specific-bootdisk.sh \
+        --master-plist-url="${MASTER_PLIST_URL}" \
+        --model "${DEVICE_MODEL}" \
+        --serial "${SERIAL}" \
+        --board-serial "${BOARD_SERIAL}" \
+        --uuid "${UUID}" \
+        --mac-address "${MAC_ADDRESS}" \
+        --width "${WIDTH:-1920}" \
+        --height "${HEIGHT:-1080}" \
+        --output-bootdisk "${BOOTDISK:=/home/arch/OSX-KVM/OpenCore/OpenCore.qcow2}" \
+|| exit 1 ; } \
+; ./enable-ssh.sh && /bin/bash -c ./Launch.sh
