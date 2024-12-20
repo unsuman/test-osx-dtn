4de2dae2b88cf6c1bca2573960ba3aad48cb1f2c
Ansuman Sahoo <anshumansahoo500@gmail.com>
2024-12-20 16:52:43 +0530

    changes to docker command

--- .devcontainer/start-docker-osx.sh
+++ .devcontainer/start-docker-osx.sh
@@ -5,6 +5,7 @@ sudo docker run -i \
     -p 50922:10022 \
     -p 5999:5999 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ${PWD}:/mnt/shared \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
     -e EXTRA="-display none -vnc 0.0.0.0:99,password=off" \
     -e SHORTNAME=ventura \
